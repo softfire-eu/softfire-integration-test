8dbb81878328d9027f84221537eb51906e83d28f
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-06-29 19:07:24 +0200

    add integration test exception

---
+++ eu/softfire/integrationtest/utils/exceptions.py
@@ -0,0 +1,12 @@
+class _BaseException(Exception):
+    def __init__(self, message, *args) -> None:
+        super().__init__(*args)
+        self.message = message
+
+
+class IntegrationTestException(_BaseException):
+    pass
+
+
+class NfvValidationException(IntegrationTestException):
+    pass
