cb1afc9bd1023ac3789f08112b11909c98bd2ba8
d.carmignani <d.carmignani@reply.it>
2017-07-03 13:58:04 +0200

    SecurityResource added

--- eu/softfire/integrationtest/utils/exceptions.py
+++ eu/softfire/integrationtest/utils/exceptions.py
@@ -14,3 +14,6 @@ class NfvValidationException(IntegrationTestException):
 
 class SdnValidationException(IntegrationTestException):
     pass
+
+class SecurityResourceValidationException(IntegrationTestException):
+    pass
