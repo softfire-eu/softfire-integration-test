4feea3b852e952eb457ab5090bc5a0a50296fa55
mrossi80 <mrossi80@gmail.com>
2017-07-10 22:06:02 +0200

    tweaking timeouts and retries for monitoring test

--- eu/softfire/integrationtest/validators/MonitoringResource.py
+++ eu/softfire/integrationtest/validators/MonitoringResource.py
@@ -36,7 +36,8 @@ class MonitoringResourceValidator(AbstractValidator):
                 exception_data = traceback.format_exc().splitlines()
                 exception_text = "Error: {}".format(exception_data[-1])
                 log.error(exception_text)
-                raise e
+                if cnt>3:
+                    raise e #raise exceptions only after 3 attempts, to allow test passing in slow environments
 
             cnt += 1
             if cnt > 4:
