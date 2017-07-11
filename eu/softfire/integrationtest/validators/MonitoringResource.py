848044c833e6324f813e35098224b40f3a3a6eda
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-07-11 19:29:56 +0200

    increased attempts

--- eu/softfire/integrationtest/validators/MonitoringResource.py
+++ eu/softfire/integrationtest/validators/MonitoringResource.py
@@ -1,11 +1,13 @@
 import json
 import logging
 import time
+import traceback
 from json import JSONDecodeError
 
 import requests
 
 from eu.softfire.integrationtest.utils.exceptions import MonitoringResourceValidationException
+from eu.softfire.integrationtest.utils.utils import get_config_value
 from eu.softfire.integrationtest.validators.validators import AbstractValidator
 
 log = logging.getLogger(__name__)
@@ -15,13 +17,15 @@ class MonitoringResourceValidator(AbstractValidator):
     def validate(self, resource, resource_id):
         log.debug('Validate MonitoringResource with resource_id: {}'.format(resource_id))
         log.debug('Validate MonitoringResource with resource: {}'.format(resource))
+
+        attempts = int(get_config_value('monitoring-resource', 'attempts', '10'))
         try:
             res = json.loads(resource)
         except JSONDecodeError as e:
             raise MonitoringResourceValidationException(e.msg)
 
         cnt = 1
-        while True:
+        while cnt <= attempts:
 
             log.debug('Validate attempt: {}'.format(cnt))
 
@@ -32,16 +36,15 @@ class MonitoringResourceValidator(AbstractValidator):
                         log.debug('********SUCCESSS*********')
                         return
             except Exception as e:
-                import traceback
-                exception_data = traceback.format_exc().splitlines()
-                exception_text = "Error: {}".format(exception_data[-1])
-                log.error(exception_text)
-                if cnt>3:
-                    raise e #raise exceptions only after 3 attempts, to allow test passing in slow environments
+                if cnt > attempts:
+                    log.error("after %d attempts zabbix is not started yet, considering it failed..." % attempts)
+                    exception_data = traceback.format_exc().splitlines()
+                    exception_text = "Error: {}".format(exception_data[-1])
+                    log.error(exception_text)
+                    raise e  # raise exceptions only after 3 attempts, to allow test passing in slow environments
 
             cnt += 1
-            if cnt > 4:
-                break
+
             time.sleep(5)
 
         raise MonitoringResourceValidationException(res)
